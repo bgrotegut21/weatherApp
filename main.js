db89fb819e1f2412d0079d3d49863a26cd40a68c
Brayden Grotegut <bgrotegut21@gmail.com>
2022-02-24 16:29:22 -0700

    working on fetching data

--- main.js
+++ main.js
@@ -1,21 +1,13 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
+/***/ "./src/data.js":
+/*!*********************!*\
+  !*** ./src/data.js ***!
+  \*********************/
 /***/ (() => {
 
-eval("console.log('something');\n\n\n//# sourceURL=webpack://weatherapp/./src/index.js?");
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /Users/braydengrotegut/Documents/webProjects/weatherApp/src/data.js: Missing semicolon. (30:15)\n\n\u001b[0m \u001b[90m 28 |\u001b[39m     \u001b[36mif\u001b[39m (\u001b[33m!\u001b[39misSame) {\u001b[0m\n\u001b[0m \u001b[90m 29 |\u001b[39m\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 30 |\u001b[39m     } lsereturn \u001b[36mfalse\u001b[39m\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m    |\u001b[39m                \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 31 |\u001b[39m   }\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 32 |\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 33 |\u001b[39m   \u001b[36mconst\u001b[39m getCoordinatesUrl \u001b[33m=\u001b[39m (city) \u001b[33m=>\u001b[39m \u001b[32m`lat=${city.lat}&lon=${city.long}`\u001b[39m\u001b[33m;\u001b[39m\u001b[0m\n    at Parser._raise (/Users/braydengrotegut/Documents/webProjects/weatherApp/node_modules/@babel/parser/lib/index.js:476:17)\n    at Parser.raiseWithData (/Users/braydengrotegut/Documents/webProjects/weatherApp/node_modules/@babel/parser/lib/index.js:469:17)\n    at Parser.raise (/Users/braydengrotegut/Documents/webProjects/weatherApp/node_modules/@babel/parser/lib/index.js:430:17)\n    at Parser.semicolon (/Users/braydengrotegut/Documents/webProjects/weatherApp/node_modules/@babel/parser/lib/index.js:3767:10)\n    at Parser.parseExpressionStatement (/Users/braydengrotegut/Documents/webProjects/weatherApp/node_modules/@babel/parser/lib/index.js:14697:10)\n    at Parser.parseStatementContent (/Users/braydengrotegut/Documents/webProjects/weatherApp/node_modules/@babel/parser/lib/index.js:14261:19)\n    at Parser.parseStatement (/Users/braydengrotegut/Documents/webProjects/weatherApp/node_modules/@babel/parser/lib/index.js:14113:17)\n    at Parser.parseBlockOrModuleBlockBody (/Users/braydengrotegut/Documents/webProjects/weatherApp/node_modules/@babel/parser/lib/index.js:14739:25)\n    at Parser.parseBlockBody (/Users/braydengrotegut/Documents/webProjects/weatherApp/node_modules/@babel/parser/lib/index.js:14730:10)\n    at Parser.parseBlock (/Users/braydengrotegut/Documents/webProjects/weatherApp/node_modules/@babel/parser/lib/index.js:14714:10)");
 
 /***/ })
 
@@ -24,9 +16,10 @@ eval("console.log('something');\n\n\n//# sourceURL=webpack://weatherapp/./src/in
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	__webpack_modules__["./src/data.js"]();
 /******/ 	
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7O1VDQUE7VUFDQTtVQUNBO1VBQ0E7VUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3dlYXRoZXJhcHAvd2VicGFjay9iZWZvcmUtc3RhcnR1cCIsIndlYnBhY2s6Ly93ZWF0aGVyYXBwL3dlYnBhY2svc3RhcnR1cCIsIndlYnBhY2s6Ly93ZWF0aGVyYXBwL3dlYnBhY2svYWZ0ZXItc3RhcnR1cCJdLCJzb3VyY2VzQ29udGVudCI6WyIiLCIvLyBzdGFydHVwXG4vLyBMb2FkIGVudHJ5IG1vZHVsZSBhbmQgcmV0dXJuIGV4cG9ydHNcbi8vIFRoaXMgZW50cnkgbW9kdWxlIGRvZXNuJ3QgdGVsbCBhYm91dCBpdCdzIHRvcC1sZXZlbCBkZWNsYXJhdGlvbnMgc28gaXQgY2FuJ3QgYmUgaW5saW5lZFxudmFyIF9fd2VicGFja19leHBvcnRzX18gPSB7fTtcbl9fd2VicGFja19tb2R1bGVzX19bXCIuL3NyYy9kYXRhLmpzXCJdKCk7XG4iLCIiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
